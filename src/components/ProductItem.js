900c8519f21301c000621cd0e6d91444d5743348
Kuba Szóstko <kubaszostko@gmail.com>
2020-08-28 17:59:35 +0200

    created ProductItem component (only HTML structure)

---
+++ src/components/ProductItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProductItem() {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <div>
+                <h2>Product Name</h2>
+                <span>Price</span>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
