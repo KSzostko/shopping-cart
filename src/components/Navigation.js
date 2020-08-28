ebc68d96a930ca7977b818723b881e726ed7af8f
Kuba Szóstko <kubaszostko@gmail.com>
2020-08-28 10:38:33 +0200

    added partial styles for Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+    box-shadow: 0 2px 2px #bbb;
+`;
+
+const StyledList = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: center;
+`;
+
+const StyledListItem = styled.li`
+    margin: 0 1rem;
+`;
+
+const StyledLink = styled.a`
+    display: block;
+    text-decoration: none;
+    color: #000;
+`;
 
 function Navigation() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="">Store</a>
-                </li>
-                <li>
-                    <a href="">Cart</a>
-                </li>
-            </ul>
-        </nav>
+        <StyledNav>
+            <StyledList>
+                <StyledListItem>
+                    <StyledLink href="">Store</StyledLink>
+                </StyledListItem>
+                <StyledListItem>
+                    <StyledLink href="">Cart</StyledLink>
+                </StyledListItem>
+            </StyledList>
+        </StyledNav>
     );
 }
 
